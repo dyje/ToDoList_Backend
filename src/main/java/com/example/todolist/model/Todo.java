6e986c2ebaa61bcb47b558e158445691353bd5ba
dyje <dyje@oocl.com>
2021-08-19 18:12:51 +0800

    [ToDoList_Frontend_and_Backend][Jesse] feat: implementation of Todo model

---
+++ src/main/java/com/example/todolist/model/Todo.java
@@ -0,0 +1,41 @@
+package com.example.todolist.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String text;
+    private boolean done;
+
+    public Todo(Integer id, String text, boolean done) {
+        this.id = id;
+        this.text = text;
+        this.done = done;
+    }
+
+    public Todo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+}
